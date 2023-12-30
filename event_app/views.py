9293690e68906d2a8916bb6c0ef17c49986a5abc
Raghib6 <raghibshahriar6@gmail.com>
2023-12-30 22:19:05 +0600

    Added User Registration, Login and Logout features

--- event_app/views.py
+++ event_app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignUpForm
+from django.contrib.auth import authenticate
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+@unauthenticated_user
+def signup(request):
+    form = UserSignUpForm()
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Congratulations! Your account creation successfull"
+            )
+            return redirect("login")
+    return render(request, "signup.html", {"form": form})
+
+
+@unauthenticated_user
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        user = authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            # messages.success(request, "You are now logged in")
+            return redirect("/home/")
+        else:
+            messages.error(request, "Invalid Login credential")
+            return redirect("login")
+    return render(request, "login.html")
+
+
+@login_required(login_url="login")
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You are logged out")
+    return redirect("login")

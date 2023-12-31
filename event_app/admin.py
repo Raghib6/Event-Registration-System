4472998fca7a3afe56a5e502aa42932ef771f3fc
Raghib6 <raghibshahriar6@gmail.com>
2023-12-31 11:40:23 +0600

    Added Event, EventRegistration models and related configurations

--- event_app/admin.py
+++ event_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Event, EventRegistration
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "time",
+        "date",
+        "available_slots",
+        "total_slots",
+        "created_at",
+    ]
+    search_fields = ["title"]
+
+
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "event",
+        "created_at",
+    ]
--- event_app/models.py
+++ event_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    time = models.TimeField(auto_now_add=False)
+    date = models.DateField(auto_now_add=False)
+    location = models.CharField(max_length=250)
+    total_slots = models.PositiveIntegerField()
+    available_slots = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="events")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class EventRegistration(models.Model):
+    user = models.ForeignKey(User, related_name="events", on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.event.title
